6998b9c0562fd6dc2da29f28be077418c06aaf99
Robert Stepanov <robertdstepanov@gmail.com>
2020-01-27 18:28:00 -0500

    3 tests passing

--- star-wars-app/src/components/StarWarsCharacters.test.js
+++ star-wars-app/src/components/StarWarsCharacters.test.js
@@ -1,13 +1,25 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, getByText, fireEvent } from '@testing-library/react';
 import StarWarsCharacters from './StarWarsCharacters';
 
 
-test('renders the SW Characters', async () => {
+test('renders the SW Characters with previous and next buttons', async () => {
 
-  const result = render(<StarWarsCharacters/>);
+  const { getByText } = render(<StarWarsCharacters/>);
+  
+  const previousButton = getByText(/previous/i);
+  const nextButton = getByText(/next/i);
+  
+});
 
+test('checks for previous and next buttons click', async () => {
+
+  const { getByText } = render(<StarWarsCharacters/>);
+
+  const previousButtonClick = { button: 0 };
+  const nextButtonClick = { button: 0 }
+  
+  fireEvent.click(getByText('Previous'));
+  fireEvent.click(getByText('Next'));
 
 })
-  
-
