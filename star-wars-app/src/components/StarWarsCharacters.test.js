2397c6d1841bc04d06ce621d1513231789e92939
Robert Stepanov <robertdstepanov@gmail.com>
2020-01-27 16:23:24 -0500

    Initial Commit - 1 test passed

---
+++ star-wars-app/src/components/StarWarsCharacters.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import StarWarsCharacters from './StarWarsCharacters';
+
+
+test('renders the SW Characters', async () => {
+
+  const result = render(<StarWarsCharacters/>);
+
+
+})
+  
+
